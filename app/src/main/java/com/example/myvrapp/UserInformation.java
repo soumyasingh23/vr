bfd4e93a1578a8e90c104aa2cfddd02f393dbb70
Soumya_Singh <soumya.singh@lendingkart.com>
2019-04-19 16:43:31 +0530

    saving data

---
+++ app/src/main/java/com/example/myvrapp/UserInformation.java
@@ -0,0 +1,35 @@
+package com.example.myvrapp;
+
+public class UserInformation {
+
+    private String visitTemple;
+    private String numberOfHoursSlept;
+    private String appetite;
+
+    public UserInformation() {
+    }
+
+    public String getVisitTemple() {
+        return visitTemple;
+    }
+
+    public void setVisitTemple(String visitTemple) {
+        this.visitTemple = visitTemple;
+    }
+
+    public String getNumberOfHoursSlept() {
+        return numberOfHoursSlept;
+    }
+
+    public void setNumberOfHoursSlept(String numberOfHoursSlept) {
+        this.numberOfHoursSlept = numberOfHoursSlept;
+    }
+
+    public String getAppetite() {
+        return appetite;
+    }
+
+    public void setAppetite(String appetite) {
+        this.appetite = appetite;
+    }
+}
--- app/src/main/java/com/example/myvrapp/data_collection.java
+++ app/src/main/java/com/example/myvrapp/data_collection.java
@@ -15,6 +15,8 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
@@ -23,13 +25,19 @@ import java.lang.reflect.Array;
 public class data_collection extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
     Button logout, display;
     TextView userName;
+    private Spinner templev, slept, appetite;
+    boolean doubleBackToExitPressedOnce = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_collection);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("UserInfo");
+
 
         userName = (TextView) findViewById(R.id.userName);
         logout = (Button)findViewById(R.id.logoutBtn);
@@ -43,15 +51,15 @@ public class data_collection extends AppCompatActivity {
         if(user != null)
         userName.setText("Hello, " + user.getDisplayName());
 
-        Spinner templev = (Spinner) findViewById(R.id.spinner1);
+        templev = (Spinner) findViewById(R.id.spinner1);
         ArrayAdapter<String> myadapter = new ArrayAdapter<>(data_collection.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.choices));
         myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item );
         templev.setAdapter(myadapter);
-        Spinner sleept = (Spinner)findViewById(R.id.spinner2);
+        slept = (Spinner)findViewById(R.id.spinner2);
         ArrayAdapter<String> myadapter2 = new ArrayAdapter<>(data_collection.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.hours));
         myadapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sleept.setAdapter(myadapter2);
-        Spinner appetite = (Spinner)findViewById(R.id.spinner3);
+        slept.setAdapter(myadapter2);
+        appetite = (Spinner)findViewById(R.id.spinner3);
         ArrayAdapter<String> myadapter3 = new ArrayAdapter<>(data_collection.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.appetite));
         myadapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         appetite.setAdapter(myadapter3);
@@ -69,13 +77,28 @@ public class data_collection extends AppCompatActivity {
         display.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://scapic.com/@riyasahal/temple_1-k0zZp"));
-                startActivity(browserIntent);
+                saveUserInformation();
+//                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://scapic.com/@riyasahal/temple_1-k0zZp"));
+//                startActivity(browserIntent);
             }
         });
 
     }
-    boolean doubleBackToExitPressedOnce = false;
+
+    private void saveUserInformation()
+    {
+        String visitTemple = templev.getSelectedItem().toString();
+        String hoursSlept =  slept.getSelectedItem().toString();
+        String appetiteStr = appetite.getSelectedItem().toString();
+        UserInformation userInformation = new UserInformation();
+        userInformation.setVisitTemple(visitTemple);
+        userInformation.setAppetite(appetiteStr);
+        userInformation.setNumberOfHoursSlept(hoursSlept);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        databaseReference.child(user.getUid()).setValue(userInformation);
+        Toast.makeText(getApplicationContext(), "info saved", Toast.LENGTH_LONG).show();
+    }
 
     @Override
     public void onBackPressed() {
