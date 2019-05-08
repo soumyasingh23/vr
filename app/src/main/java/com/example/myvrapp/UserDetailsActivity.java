b0735d29aad6ede7eb48ffe805c22d1c90115d19
Soumya_Singh <soumya.singh@lendingkart.com>
2019-05-08 22:17:27 +0530

    modified user details on doc side

--- app/src/main/java/com/example/myvrapp/UserDetailsActivity.java
+++ app/src/main/java/com/example/myvrapp/UserDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.myvrapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,54 +24,33 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class UserDetailsActivity extends AppCompatActivity {
 
     TextView userName, psychBefore, psychAfter, vrInvolvement, commSupp, healthBefore, healthAfter, timeBalBefore, timeBalAfter, HIbefore, HIafter;
-    private Button back, logout;
+    private Button logout;
     DatabaseReference databaseReference;
     private FirebaseAuth firebaseAuth;
+    private BarChart barChart;
+    private UserInformation userBefore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
-        UserInformation user = (UserInformation) getIntent().getSerializableExtra("user");
+        userBefore = (UserInformation) getIntent().getSerializableExtra("user");
         firebaseAuth = FirebaseAuth.getInstance();
+        barChart = (BarChart) findViewById(R.id.chart);
         userName = (TextView) findViewById(R.id.userName3);
-        userName.setText(user.getName().toUpperCase());
-
-        commSupp = (TextView) findViewById(R.id.commSupp);
-        commSupp.setText(user.getCommunitySupport());
-
-        healthBefore = (TextView) findViewById(R.id.health);
-        healthBefore.setText(user.getHealthAndEnergyLevel());
-
-        psychBefore = (TextView) findViewById(R.id.psych);
-        psychBefore.setText(user.getPsychologicalWellBeing());
-
-        timeBalBefore = (TextView) findViewById(R.id.timeBal);
-        timeBalBefore.setText(user.getTimeBalance());
-
-        float HIb = (Float.parseFloat(user.getCommunitySupport()) + Float.parseFloat(user.getPsychologicalWellBeing()) + Float.parseFloat(user.getHealthAndEnergyLevel()) + Float.parseFloat(user.getTimeBalance()))/4;
+        userName.setText(userBefore.getName().toUpperCase());
+        float HIb = (Float.parseFloat(userBefore.getCommunitySupport()) + Float.parseFloat(userBefore.getPsychologicalWellBeing()) + Float.parseFloat(userBefore.getHealthAndEnergyLevel()) + Float.parseFloat(userBefore.getTimeBalance()))/4;
         HIbefore = (TextView) findViewById(R.id.HappInd);
         HIbefore.setText(Float.toString(HIb));
-
-        databaseReference = FirebaseDatabase.getInstance().getReference("PostUseInfo").child(user.getUserId());
-
-        psychAfter = (TextView) findViewById(R.id.psychAfter);
-        vrInvolvement = (TextView) findViewById(R.id.Involvement);
-        healthAfter = (TextView) findViewById(R.id.healthAfter);
-        timeBalAfter = (TextView) findViewById(R.id.timeBalAfter);
+        databaseReference = FirebaseDatabase.getInstance().getReference("PostUseInfo").child(userBefore.getUserId());
         HIafter = (TextView) findViewById(R.id.HappIndAfter);
 
-        back = (Button) findViewById(R.id.backBtn2);
         logout = (Button) findViewById(R.id.logoutBtn3);
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), DoctorActivity.class));
-            }
-        });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,10 +72,43 @@ public class UserDetailsActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 UserInformation user = dataSnapshot.getValue(UserInformation.class);
-                psychAfter.setText(user.getPsychologicalWellBeing());
-                vrInvolvement.setText(user.getVrInvolvement());
-                healthAfter.setText(user.getHealthAndEnergyLevel());
-                timeBalAfter.setText(user.getTimeBalance());
+
+                ArrayList barGroup1 = new ArrayList();
+                barGroup1.add(new BarEntry(Integer.parseInt(userBefore.getCommunitySupport() == null ? "0" : userBefore.getCommunitySupport()), 0));
+                barGroup1.add(new BarEntry(Integer.parseInt(userBefore.getHealthAndEnergyLevel() == null ? "0" : userBefore.getHealthAndEnergyLevel()), 1));
+                barGroup1.add(new BarEntry(Integer.parseInt(userBefore.getPsychologicalWellBeing() == null ? "0" : userBefore.getPsychologicalWellBeing()), 2));
+                barGroup1.add(new BarEntry(Integer.parseInt(userBefore.getTimeBalance() == null ? "0" : userBefore.getTimeBalance()), 3));
+                barGroup1.add(new BarEntry(0, 4));
+
+
+                ArrayList barGroup2 = new ArrayList();
+                barGroup2.add(new BarEntry(0, 0));
+                barGroup2.add(new BarEntry(Integer.parseInt(user.getHealthAndEnergyLevel() == null ? "0" : user.getHealthAndEnergyLevel()), 1));
+                barGroup2.add(new BarEntry(Integer.parseInt(user.getPsychologicalWellBeing() == null ? "0" : user.getPsychologicalWellBeing()), 2));
+                barGroup2.add(new BarEntry(Integer.parseInt(user.getTimeBalance() == null ? "0" : user.getTimeBalance()), 3));
+                barGroup2.add(new BarEntry(Integer.parseInt(user.getVrInvolvement() == null ? "0" : user.getVrInvolvement()), 4));
+
+                BarDataSet barDataSet1 = new BarDataSet(barGroup1, "Before");
+                barDataSet1.setColor(Color.rgb(159, 217, 242));
+                BarDataSet barDataSet2 = new BarDataSet(barGroup2, "After");
+                barDataSet2.setColor(Color.rgb(100, 229, 125));
+
+                final ArrayList<String> labels = new ArrayList();
+                labels.add("CS");
+                labels.add("H&E");
+                labels.add("Psych");
+                labels.add("Time");
+                labels.add("VR");
+
+                ArrayList dataSets = new ArrayList();
+                dataSets.add(barDataSet1);
+                dataSets.add(barDataSet2);
+
+                BarData data = new BarData(labels, dataSets);
+                barChart.setData(data);
+                barChart.setDescription("User details");
+                barChart.animateXY(2000, 2000);
+                barChart.invalidate();
                 float HIa = (Float.parseFloat(user.getVrInvolvement()) + Float.parseFloat(user.getPsychologicalWellBeing()) + Float.parseFloat(user.getHealthAndEnergyLevel()) + Float.parseFloat(user.getTimeBalance()))/4;
                 HIafter.setText(Float.toString(HIa));
             }
